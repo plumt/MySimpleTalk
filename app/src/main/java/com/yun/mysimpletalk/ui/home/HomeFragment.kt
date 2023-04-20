2d84abb413c46ba94edfdd3686a5e93c18e33129
yslee <yslee@openit.co.kr>
2023-04-20 10:59:50 +0900

    1. 친구 등록 기능 추가

--- app/src/main/java/com/yun/mysimpletalk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yun/mysimpletalk/ui/home/HomeFragment.kt
@@ -3,11 +3,16 @@ package com.yun.mysimpletalk.ui.home
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 import com.yun.mysimpletalk.R
 import com.yun.mysimpletalk.BR
 import com.yun.mysimpletalk.base.BaseFragment
+import com.yun.mysimpletalk.common.constants.FirebaseConstants.Path.USER
 import com.yun.mysimpletalk.databinding.FragmentHomeBinding
+import com.yun.mysimpletalk.ui.dialog.EdittextDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,12 +28,61 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        sharedViewModel.let {  sv ->
-            sv.userInfo.observe(viewLifecycleOwner){
-                Log.d("lys","userInfo > $it")
+        sharedViewModel.let { sv ->
+            sv.userInfo.observe(viewLifecycleOwner) {
+                Log.d("lys", "userInfo > $it")
             }
 
             sv.showBottomNav()
         }
+
+        binding.let { v ->
+            v.btnAddFriend.setOnClickListener {
+                searchFriend()
+            }
+        }
+    }
+
+    fun searchFriend() {
+        EdittextDialog().run {
+            showDialog(requireActivity(), "검색", "검색하실 닉네임을 입력해 주세요")
+            setDialogListener(object : EdittextDialog.CustomDialogListener {
+                override fun onResult(result: String) {
+                    nickNameCheck(result) { friendId ->
+                        if (friendId != "") {
+                            dismissDialog()
+                            addFriend(friendId)
+                        } else {
+                            Toast.makeText(
+                                requireActivity(),
+                                "해당 닉네임을 가지고 있는 사용자가 존재하지 않습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    fun nickNameCheck(nickName: String, callBack: (String) -> Unit) {
+        FirebaseFirestore.getInstance().collection(USER)
+            .whereEqualTo("name", nickName)
+            .get()
+            .addOnSuccessListener { documents ->
+                if(!documents.isEmpty && nickName != sharedViewModel.userInfo.value!!.nickName){
+                    callBack(documents.documents[0].id)
+                } else callBack("")
+            }
+            .addOnFailureListener { callBack("") }
+    }
+
+    fun addFriend(friendId: String) {
+        FirebaseFirestore.getInstance().collection(USER)
+            .document(friendId)
+            .update("wait", FieldValue.arrayUnion(sharedViewModel.userInfo.value!!.userId))
+            .addOnCompleteListener {
+                Log.d("lys", "add Friends > ${it.isSuccessful}")
+            }
     }
 }
